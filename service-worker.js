fd2daf674b3dd4e303c68b21e6f9657add8a721e
TheGameOnAddict <96160502+TheGameOnAddict@users.noreply.github.com>
2026-06-30 00:07:32 -0400

    Layout

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,2 @@
-const CACHE_NAME='pdf-markup-flex-layout-v1';
-const APP_FILES=['./','index.html','manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_FILES)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>self.clients.claim());
